d63d2e8be9738ae5029b9302dc05e0343711430d
Frederik Hannibal <frederik@backhand.dk>
2019-11-13 13:58:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,8 @@ docker_run="docker run"
 docker_run="$docker_run -e POSTGRES_DB=$INPUT_POSTGRESQL_DB"
 docker_run="$docker_run -e POSTGRES_USER=$INPUT_POSTGRESQL_USER"
 docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
-
-if [ ! -z "$INPUT_POSTGRESQL_INIT_SCRIPTS" ]
-then
-  PWD=`pwd`
-  docker_run="$docker_run -v $PWD/$INPUT_POSTGRESQL_INIT_SCRIPTS:/docker-entrypoint-initdb.d"
-fi
+PWD=`pwd`
+docker_run="$docker_run -v $PWD/$INPUT_POSTGRESQL_INIT_SCRIPTS:/docker-entrypoint-initdb.d"
 
 docker_run="$docker_run -d -p 5432:5432 postgres:$INPUT_POSTGRESQL_VERSION"
 
